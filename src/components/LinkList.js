cdd5f227cb85bd7f0647f34f91aae81bf1b4bdff
robbie <robbiekruszynski@gmail.com>
2023-10-04 17:16:58 +0200

    css file creates, adjustemnts on hover with LT

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -5,7 +5,7 @@ const LinkList = () => {
   const links = [
     { title: 'Telegram', url: 'https://t.me/caronfire', background: 'linear-gradient(45deg, #009688 30%, #4db6ac 90%)' },
     { title: 'Twitter', url: 'https://twitter.com/caronfiree', background: 'linear-gradient(45deg, #009688 30%, #4db6ac 90%)' },
-    { title: 'GitHub', url: 'https://github.com/CaronSch', background: 'linear-gradient(45deg, #009688 30%, #4db6ac 90%)' },
+   { title: 'GitHub', url: 'https://github.com/CaronSch', background: 'linear-gradient(45deg, #009688 30%, #4db6ac 90%)' },
     { title: 'LinkedIn', url: 'https://www.linkedin.com/in/caronschaller/', background: 'linear-gradient(45deg, #009688 30%, #4db6ac 90%)' },
   ];
 
